b2cc6b1d5e5bc6da5872ebf228dc741ee1752637
Kadavala Eashwar Sundhar Sai <eashwarlucky.com>
2025-08-22 21:48:51 +0530

    Drum kit

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,1 +1,3 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Eashwar";
+// document.querySelector("h1").style.color = "blue";
+// document.querySelector("button").style.backgroundColor = "blue";
